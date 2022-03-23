3c3f31c648bc730dafd42b32495dcde633a1692d
Irving Didier <bider88@gmail.com>
2022-03-23 12:21:10 -0600

    configure firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+
+import { environment } from '../environments/environment';
+
+// Angular Fire
+import { AngularFireModule } from '@angular/fire/compat';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/config-firebase.ts
+++ src/environments/config-firebase.ts
@@ -1,9 +1,9 @@
 export const firebase = {
-  projectId: '',
-  appId: '',
-  storageBucket: '',
-  apiKey: '',
-  authDomain: '',
-  messagingSenderId: '',
-  measurementId: '',
+  projectId: 'the-meal-test',
+  appId: '1:730980096988:web:02db7d6cb702d10437cd19',
+  storageBucket: 'the-meal-test.appspot.com',
+  apiKey: 'AIzaSyC1VbQBMhPHVqr8g4-Dy_CZguipSVTtzVo',
+  authDomain: 'the-meal-test.firebaseapp.com',
+  messagingSenderId: '730980096988',
+  measurementId: 'G-BR33FHHQT4',
 };
